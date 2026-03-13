f9f4c69803472f7b398118d265930e573bb442b1
ZQ <zq201x@mail.ru>
2026-03-13 10:36:39 -0700

    fix chat

--- server.js
+++ server.js
@@ -1,39 +1,29 @@
-const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-const { Server } = require("socket.io");
+const express = require("express")
+const http = require("http")
+const { Server } = require("socket.io")
 
-const io = new Server(http);
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
-app.use(express.static("public"));
-
-const users = {};
+app.use(express.static("public"))
 
 io.on("connection", (socket) => {
-  const username = "User" + Math.floor(Math.random() * 1000);
-  users[socket.id] = username;
 
-  io.emit("system", `${username} вошёл в чат`);
+console.log("user connected")
 
-  socket.on("message", (msg) => {
-    const text = String(msg || "").trim();
-    if (!text) return;
+socket.on("chat message", (msg) => {
+io.emit("chat message", msg)
+})
 
-    io.emit("message", {
-      user: users[socket.id],
-      text: text
-    });
-  });
+socket.on("disconnect", () => {
+console.log("user disconnected")
+})
 
-  socket.on("disconnect", () => {
-    const username = users[socket.id];
-    if (username) {
-      io.emit("system", `${username} вышел из чата`);
-      delete users[socket.id];
-    }
-  });
-});
+})
 
-http.listen(3000, () => {
-  console.log("Messenger running on http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => {
+console.log("Messenger running on port " + PORT)
+})
